99979ad2ac5d7d5aba1cf4d6534547583dcb630a
Luis Isasi <luis16isasi@gmail.com>
2021-06-29 20:03:48 -0500

    chore: add base header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,31 @@
+import Link from '@Components/Links/Link'
+
 const Header = () => {
-  return <header className="h-48 w-full bg-gray-400">HEADER</header>
+  return (
+    <header className="h-14 w-full bg-gray-400 flex justify-between items-center lg:mx-auto lg:max-w-6xl">
+      <h1>Luis Isasi</h1>
+      <div className="flex items-center">
+        <nav className="bg-blue-400">
+          <Link href="/blog" className="mr-8">
+            Blog
+          </Link>
+          <Link href="/snippets" className="mr-8">
+            Snippets
+          </Link>
+          <Link href="/experimentos" className="mr-8">
+            Experimentos
+          </Link>
+          <Link href="/aboutme" className="mr-8">
+            Sobre mí
+          </Link>
+          <Link href="/contactame" className="mr-8">
+            Contáctame
+          </Link>
+        </nav>
+        <button className=""></button>
+      </div>
+    </header>
+  )
 }
 
 export default Header
