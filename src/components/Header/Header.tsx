c638a939316be59880472e02c334ef5a8cb4d4b9
Luis Isasi <luis16isasi@gmail.com>
2021-07-08 11:14:29 -0500

    style: add hover in nav's links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,12 +12,13 @@ const Header = () => {
   const isLaptop = useResponsive({
     minMediaQuery: MEDIAQUERY_LG,
   })
-
   return (
-    <header className="w-full h-auto text-pink-600 dark:text-pink-500  bg-gradient-to-r from-gradient-start to-gradient-end dark:from-dark-secondary dark:to-dark-secondary">
+    <header className="w-full h-auto bg-gradient-to-r from-gradient-start to-gradient-end dark:from-dark-secondary dark:to-dark-secondary">
       <div className="h-14 w-full  px-5 lg:px-0 flex justify-between items-center lg:mx-auto lg:max-w-5xl">
         <Link href="/">
-          <h1 className="font-bold text-2xl">Luis Isasi</h1>
+          <h1 className="font-bold text-2xl text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-600">
+            Luis Isasi
+          </h1>
         </Link>
         <div className="flex items-center">
           {isMovilAndTablet && (
--- src/components/Header/components/Nav.tsx
+++ src/components/Header/components/Nav.tsx
@@ -4,19 +4,34 @@ import BtnDarkMode from '@Components/Buttons/BtnDarkMode'
 const Nav = () => {
   return (
     <nav className="font-bold flex items-center">
-      <Link href="/blog" className="p-2 mr-6">
+      <Link
+        href="/blog"
+        className="p-2 mr-6 text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-400"
+      >
         Blog
       </Link>
-      <Link href="/snippets" className="p-2 mr-6">
+      <Link
+        href="/snippets"
+        className="p-2 mr-6 text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-600"
+      >
         Snippets
       </Link>
-      <Link href="/experimentos" className="p-2 mr-6">
+      <Link
+        href="/experimentos"
+        className="p-2 mr-6 text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-600"
+      >
         Experimentos
       </Link>
-      <Link href="/aboutme" className="p-2 mr-6">
+      <Link
+        href="/aboutme"
+        className="p-2 mr-6 text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-600"
+      >
         Sobre mí
       </Link>
-      <Link href="/contactame" className="p-2 mr-6">
+      <Link
+        href="/contactame"
+        className="p-2 mr-6 text-dark-light hover:text-dark dark:text-pink-500 dark:hover:text-pink-600"
+      >
         Contáctame
       </Link>
       <BtnDarkMode />
