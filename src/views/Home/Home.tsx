0ad90836db28702206d6e428171cd6a790696a8b
Luis Isasi <luis16isasi@gmail.com>
2021-07-08 00:32:16 -0500

    style: add dark mode in blogs and categories

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,15 +6,15 @@ import Categories from './components/Categories'
 
 const Home = () => {
   return (
-    <div className="w-full h-auto">
+    <div className="w-full dark:bg-dark-primary">
       <div className="w-full h-auto bg-gradient-to-r from-gradient-start to-gradient-end dark:from-dark-secondary dark:to-dark-secondary">
         <div className="relative h-auto w-full md:max-w-3xl lg:max-w-4xl xl:max-w-5xl p-5 md:pt-5 md:pb-0 lg:px-0 mx-auto xl:flex">
           <PresentingMe />
           <Technologies />
         </div>
-        <Wave />
+        <Wave className="text-white dark:text-dark-primary" />
       </div>
-      <div className="h-auto w-full flex flex-col lg:flex-row md:max-w-3xl lg:max-w-4xl xl:max-w-5xl p-5 md:pt-5 md:pb-0 lg:px-0 mx-auto ">
+      <div className="h-auto w-full flex flex-col lg:flex-row lg:justify-between md:max-w-3xl lg:max-w-4xl xl:max-w-5xl p-5 md:pt-5 md:pb-0 lg:px-0 mx-auto ">
         <RecentContent />
         <Categories />
       </div>
--- src/views/Home/Sections/RecentContent.tsx
+++ src/views/Home/Sections/RecentContent.tsx
@@ -2,12 +2,14 @@ const RecentContent = () => {
   const renderArticle = () => {
     return Array.from({ length: 7 }).map(() => {
       return (
-        <article className="rounded-md py-6">
-          <header className="font-bold text-xl">
+        <article className="rounded-md py-8">
+          <header className="font-bold text-xl dark:text-white">
             ¿Cómo leer y modificar archivos y carpetas en golang?
           </header>
-          <time className="text-sm font-bold text-gray-400">29/06/2021</time>
-          <p className="py-3">
+          <time className="text-sm font-bold text-gray-400 dark:text-gray-500">
+            29/06/2021
+          </time>
+          <p className="py-3 dark:text-white">
             Hace unas semanas tuve la necesidad de recorrer miles de archivos y
             carpetas, leer su contenido y luego contabilizar cuántos de estos
             cumplían con cierto requerimiento. Al inicio pensaba usar nodejs
@@ -22,7 +24,7 @@ const RecentContent = () => {
   }
 
   return (
-    <section className="">
+    <section className="w-full lg:max-w-2xl">
       <p className="text-sm font-bold py-5 text-pink-600">
         CONTENIDO RECIENTE:
       </p>
