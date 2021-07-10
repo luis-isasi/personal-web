9e93dc497582f76b2bae88baaeada4f38592fb85
Luis Isasi <luis16isasi@gmail.com>
2021-07-10 01:08:25 -0500

    chore: add new pages

---
+++ src/pages/contactme.tsx
@@ -0,0 +1,13 @@
+const contactme = () => {
+  return <div></div>
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      isConstruction: true,
+    },
+  }
+}
+
+export default contactme
---
+++ src/pages/snippets.tsx
@@ -0,0 +1,13 @@
+const snippets = () => {
+  return <div></div>
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      isConstruction: true,
+    },
+  }
+}
+
+export default snippets
