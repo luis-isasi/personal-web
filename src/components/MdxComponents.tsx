e3a7af2bd5170cc753f5e574df3a75483f3e02f0
Luis Isasi <luis16isasi@gmial.com>
2021-07-17 01:31:30 -0500

    style: add dark mode for code snippet

--- src/components/MdxComponents.tsx
+++ src/components/MdxComponents.tsx
@@ -1,15 +1,26 @@
-export const H2: React.FC = ({ children }) => {
-  return <h2 className="text-3xl text-red-600 dark:text-white">{children}</h2>
-}
-
-export const Blockquote: React.FC = ({ children }) => {
-  return <blockquote className="bg-gray-700">{children}</blockquote>
-}
-
-export const StaticCode: React.FC = ({ children }) => {
+export const H3: React.FC = ({ children }) => {
   return (
-    <div className="bg-gray-500 dark:bg-gray-800">
-      <pre>{children}</pre>
-    </div>
+    <h3 className="text-xl lg:text-2xl xl:text-3xl mt-10 font-bold text-blue-700 dark:text-green-500">
+      {children}
+    </h3>
+  )
+}
+
+export const P: React.FC = ({ children }) => {
+  return (
+    <p className="my-4 text-sm md:text-base lg:text-lg text-dark-light dark:text-white">
+      {children}
+    </p>
+  )
+}
+
+export const A: React.FC = ({ children }) => {
+  return (
+    <a
+      target="_blank"
+      className="text-green-500 dark:text-gray-400 underline cursor-pointer"
+    >
+      {children}
+    </a>
   )
 }
