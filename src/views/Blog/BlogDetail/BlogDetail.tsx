be86faddb2603ae39ffd8180ac2b17e0aa83cb18
Luis Isasi <luis16isasi@gmial.com>
2021-07-17 01:31:30 -0500

    style: add dark mode for code snippet

--- src/views/Blog/BlogDetail/BlogDetail.tsx
+++ src/views/Blog/BlogDetail/BlogDetail.tsx
@@ -2,12 +2,13 @@ import Head from 'next/head'
 import { TypeBlogDetail } from '@Types'
 import { MDXRemote } from 'next-mdx-remote'
 import { MDXProvider, MDXProviderProps } from '@mdx-js/react'
-import { H2, Blockquote, StaticCode } from '@Components/MdxComponents'
+import { H3, P, A } from '@Components/MdxComponents'
 import CodeSnippet from '@/src/components/CodeSnippet'
 
 const mdxComponents: MDXProviderProps['components'] = {
-  h2: H2,
-  blockquote: Blockquote,
+  h3: H3,
+  p: P,
+  a: A,
   code: CodeSnippet,
 }
 
@@ -15,15 +16,15 @@ const BlogDetail: React.FC<TypeBlogDetail> = ({ source, data }) => {
   return (
     <>
       <Head>
-        <title></title>
+        <title>{`${data.title} | Luis Isasi`}</title>
       </Head>
       <div className="w-full bg-white dark:bg-dark-light">
-        <div className="w-full sm:max-w-xl px-5 py-4 lg:px-0 md:max-w-2xl xl:max-w-5xl h-auto mx-auto ">
+        <div className="w-full sm:max-w-xl px-5 py-4 lg:px-0 md:max-w-2xl xl:max-w-5xl h-auto mx-auto">
           <MDXProvider components={mdxComponents}>
-            <h3 className="text-black dark:text-yellow-400 font-bold text-center text-2xl lg:text-4xl pt-7 lg:pt-10 pb-4 lg:pb-8">
+            <h2 className="text-dark-light dark:text-white font-bold text-center text-2xl lg:text-4xl p-0 lg:py-8">
               {data.title}
-            </h3>
-            <div className="">
+            </h2>
+            <div>
               <MDXRemote {...source} />
             </div>
           </MDXProvider>
