e2698756c0bbe225afbdc99f139a867d3298365e
Luis Isasi <luis16isasi@gmial.com>
2021-07-20 22:25:34 -0500

    fix: fix bug when navegating in nav

--- src/components/Header/components/Nav.tsx
+++ src/components/Header/components/Nav.tsx
@@ -1,24 +1,28 @@
 import Link from '@Components/Links/Link'
 
 const itemClass =
-  'p-2 mr-6 text-purple-400 lg:text-dark-light lg:hover:text-dark dark:text-white dark:hover:text-gray-300 transition-colors ease-in-out duration-200'
+  'p-2 mr-6 text-purple-400 text-left lg:text-dark-light lg:hover:text-dark dark:text-white dark:hover:text-gray-300 transition-colors ease-in-out duration-200'
 
-const Nav = () => {
+const Nav: React.FC<{ closeModal?: () => void }> = ({ closeModal }) => {
+  const handlerClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+    closeModal && closeModal()
+  }
+
   return (
     <nav className="font-bold flex flex-col lg:flex-row lg:items-center">
-      <Link href="/blog" className={itemClass}>
+      <Link href="/blog" className={itemClass} onClick={handlerClick}>
         Blog
       </Link>
-      <Link href="/snippets" className={itemClass}>
+      <Link href="/snippets" className={itemClass} onClick={handlerClick}>
         Snippets
       </Link>
-      <Link href="/experiments" className={itemClass}>
+      <Link href="/experiments" className={itemClass} onClick={handlerClick}>
         Experimentos
       </Link>
-      <Link href="/aboutme" className={itemClass}>
+      <Link href="/aboutme" className={itemClass} onClick={handlerClick}>
         Sobre mí
       </Link>
-      <Link href="/contactme" className={itemClass}>
+      <Link href="/contactme" className={itemClass} onClick={handlerClick}>
         Contáctame
       </Link>
     </nav>
