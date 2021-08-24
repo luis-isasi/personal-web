79112b6ada4e5280565c801be3eda442e12cd6c6
Luis Isasi <luis16isasi@gmail.com>
2021-08-24 09:37:54 -0500

    style: update styles for btn category

--- src/views/Home/components/BtnCategorie.tsx
+++ src/views/Home/components/BtnCategorie.tsx
@@ -5,7 +5,7 @@ const BtnCategorie: React.FC<Categorie> = ({ url, name }) => {
   return (
     <Link
       href={url}
-      className="bg-indigo-600 m-1 w-auto max-w-min px-3 pb-1 rounded-md text-white text-sm font-medium hover:bg-indigo-500 transition-all ease-in-out duration-150"
+      className="bg-indigo-600  w-auto max-w-min py-1 px-2 mr-1 rounded-md text-white text-sm font-medium hover:bg-indigo-500 transition-all ease-in-out duration-150"
     >
       {name}
     </Link>
