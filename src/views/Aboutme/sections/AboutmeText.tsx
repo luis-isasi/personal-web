59305395fac98121ae2bc087101e06969e260ec5
Luis Isasi <luis16isasi@gmail.com>
2021-10-10 23:14:02 -0500

    fix: dix typos in aboutme's text

--- src/views/Aboutme/sections/AboutmeText.tsx
+++ src/views/Aboutme/sections/AboutmeText.tsx
@@ -6,9 +6,9 @@ const AboutmeText = () => {
         Soy{' '}
         <b className="text-blue-600 dark:text-blue-500">Luis Isasi Oyolo,</b>{' '}
         tengo 22 años y soy un estudiante cursando el octavo ciclo de la carrera
-        de ing. de Sistemas, en los primeros ciclos me interesaba mucho el
+        de ING. De Sistemas, en los primeros ciclos me interesaba mucho el
         desarrollo web, comencé estudiando HTML5 y CSS3, como todo principiante
-        pensé que eso errá suficiente y me equivoque, seguí tomando cursos de
+        pensé que eso era suficiente y me equivoque, seguí tomando cursos de
         JavaScript, hasta ese punto descubrí que el desarrollo web era un mundo
         muy extenso, pero me agradaba y estaba dispuesto a seguir estudiando de
         la mano con la universidad,
@@ -17,10 +17,10 @@ const AboutmeText = () => {
           luego comencé con React js, react hooks, Github, styled components,
           webpack, babel, tailwind css, next js, etc.{' '}
         </i>
-        Hasta el día de hoy sigo aprendiendo en diferente plataformas virtuales,
-        tengo proyectos en mi perfil de github y estoy dispuesto a crecer
-        personalmente y profesionalmente para aportar a esta comunidad del
-        desarrollo web.
+        Hasta el día de hoy sigo aprendiendo en diferentes plataformas
+        virtuales, tengo proyectos en mi perfil de github y estoy dispuesto a
+        crecer personalmente y profesionalmente para aportar a esta comunidad
+        del desarrollo web.
       </p>
     </section>
   )
