92a82051438135e1b3211287eafaf448bf75c964
Luis Isasi <luis16isasi@gmail.com>
2021-10-17 18:01:58 -0500

    chore: add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,23 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}');
+              `,
+            }}
+          />
+        </Head>
         <body className="scrool-none scroolbar bg-scroolbar-white">
           <Main />
           <NextScript />
